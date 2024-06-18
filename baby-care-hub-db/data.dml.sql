88183a93b223315d80a6ff6011bdf4609a6212e2
Maroua SOLTANI <113924387+marouachd@users.noreply.github.com>
2024-06-18 14:10:57 +0200

    db

--- baby-care-hub-db/data.dml.sql
+++ baby-care-hub-db/data.dml.sql
@@ -1,40 +1,82 @@
+DELETE FROM actualities;
+DELETE FROM childs_activities;
+DELETE FROM naps;
+DELETE FROM baby_bottles;
+DELETE FROM meals;
+DELETE FROM childs_leisures;
+DELETE FROM snacks;
+DELETE FROM activities;
+DELETE FROM leisures;
 DELETE FROM childs;
-DELETE FROM guard_mode;
-DELETE FROM genders;
-DELETE FROM persons;
-DELETE FROM roles;
 DELETE FROM users;
+DELETE FROM persons;
+DELETE FROM guard_mode;
+DELETE FROM roles;
+DELETE FROM genders;
 
+-- Insertion des données dans les tables
 INSERT INTO genders (gender_name) VALUES 
-	('F'),
-	('M');
+    ('F'),
+    ('M');
 
 INSERT INTO roles (role_name) VALUES 
-	('Assistante maternelle'),
-	('Parent');
-	
+    ('Assistante maternelle'),
+    ('Parent');
+
 INSERT INTO guard_mode (guard_mode_name) VALUES
-	('Temps plein'),
-	('Les mercredis'),
-	('Les vacances scolaires'),
-	('Sortie d''école');
+    ('Temps plein'),
+    ('Les mercredis'),
+    ('Les vacances scolaires'),
+    ('Sortie d''école');
 
-INSERT INTO persons(pseudo_name,first_name,last_name,identity_photo_name) VALUES 
-	('mamanEyad','maroua','soltani','maroua.jpg'),
-	('bestmom','lamia','hajj','lamia.jpeg'),
-	('baby17','Eyad','chroud','');
-	
-INSERT INTO users (mail_adress, phone_number,PASSWORD,role_id,person_id) VALUES
-				('nounou@gmail.com','04785321','251145hh',1,2),
-				 ('parent@gmail.com','04785321','251145hh',2,3);
-				
-INSERT INTO childs (birthday_date, child_id, gender_id, guard_id, person_id,childminder_code) VALUES 
-                   ('2019-01-17', 'Eyad17', 2, 2, 3,1);
+INSERT INTO persons (pseudo_name, first_name, last_name, identity_photo_name) VALUES 
+    ('mamanEyad', 'maroua', 'soltani', 'maroua.jpg'),
+    ('bestmom', 'lamia', 'hajj', 'lamia.jpeg'),
+    ('baby17', 'Eyad', 'chroud', '');
 
-					
+INSERT INTO users (mail_adress, phone_number, password, role_id, person_id) VALUES
+    ('nounou@gmail.com', '04785321', '251145hh', 1, 1),
+    ('parent@gmail.com', '04785321', '251145hh', 2, 2);
 
+INSERT INTO childs (birthday_date, gender_id, guard_id, person_id, childminder_code, parent_id) VALUES 
+    ('2019-01-17', 1, 3, 3, 1, 2);
 
+INSERT INTO activities (activity_name) VALUES
+    ('Lego'), 
+    ('Puzzel'),
+    ('Coloriage'),
+    ('Dessin'),
+    ('Peinture');
 
+INSERT INTO leisures (leisure_name) VALUES
+    ('Lire un livre'), 
+    ('Danser'),
+    ('Regarder les dessins animées'),
+    ('au parc à coté'),
+    ('à la bibliothèque'),
+    ('au ludothèque');
 
-	
-	
+INSERT INTO snacks (snack_name, image_path) VALUES
+    ('Un fruit', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6ZnzI4gOfp-jmot0JloS1b3lZ8kC-e4sICQ&usqp=CAU'), 
+    ('Un yaourt', 'https://media.istockphoto.com/id/1068916210/vector/yoghurt.jpg?s=612x612&w=0&k=20&c=KmSJ-a7Ol92ZCfVkuHu6c-Idy5XUoagt5mmVg1gEGjA='),
+    ('une tartine', 'https://media.istockphoto.com/id/1128230134/vector/bread.jpg?s=612x612&w=0&k=20&c=xvAXIVc-Y9y8F5PQuRInN_378E-e3HaW1prIMrp-R38='),
+    ('un gateaux', 'https://static9.depositphotos.com/1072356/1199/v/450/depositphotos_11997141-stock-illustration-chocolate-cookies.jpg');
+
+-- Insertion des données dans les tables avec dépendances
+INSERT INTO childs_activities (activity_id, child_id, commentaire, time, date) 
+VALUES (1, 1, 'il a bien joué au puzzle', '12:20:00', '2024-03-28');
+
+INSERT INTO naps (child_id, commentaire, start_time, end_time, type, date) VALUES
+    (1, 'il a bien dormis', '12:00:00', '14:00:00', 'sieste', '2024-03-28');
+
+INSERT INTO baby_bottles (child_id, volume, time, date) VALUES
+    (1, 250.0, '12:00:00', '2024-03-28');
+
+INSERT INTO meals (child_id, snack_id, eval, commentaire, date, type)
+VALUES (3, 2, 'très bien mangé', 'plat poisson + riz + fruit', '2024-03-28', 'gouter');
+
+INSERT INTO childs_leisures (leisure_id, child_id, commentaire, date) 
+VALUES (1, 1, 'il a bien joué', '2024-03-28');
+
+INSERT INTO actualities (child_id, date, child_activity_id, baby_bottel_id, meal_id, nap_id, leisure_id)
+VALUES (1, '2024-03-28', 1, 1, 1, 1, 1);
--- baby-care-hub-db/schema.ddl.sql
+++ baby-care-hub-db/schema.ddl.sql
@@ -1,16 +1,27 @@
-DROP TABLE IF EXISTS childs;
+-- Drop tables if they exist
+DROP TABLE IF EXISTS childs CASCADE;
 DROP TABLE IF EXISTS guard_mode;
 DROP TABLE IF EXISTS genders;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS persons;
+DROP TABLE IF EXISTS persons CASCADE;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS childs_activities;
+DROP TABLE IF EXISTS naps;
+DROP TABLE IF EXISTS baby_bottles;
+DROP TABLE IF EXISTS meals CASCADE;
+DROP TABLE IF EXISTS childs_leisures;
+DROP TABLE IF EXISTS actualities;
+DROP TABLE IF EXISTS snacks;
+DROP TABLE IF EXISTS password_reset_token;
+DROP TABLE IF EXISTS histories CASCADE;
+DROP TABLE IF EXISTS leisures;
+DROP TABLE IF EXISTS childs_leisures CASCADE;
 
-
-
-
+-- Create tables
 CREATE TABLE genders (
     id SERIAL PRIMARY KEY,
-    gender_name Varchar(100)
+    gender_name VARCHAR(100)
 );
 
 CREATE TABLE roles (
@@ -33,7 +44,7 @@ CREATE TABLE persons (
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    mail_adress VARCHAR(100),
+    mail_address VARCHAR(100),
     phone_number VARCHAR(100),
     password VARCHAR(100),
     role_id INT,
@@ -42,19 +53,134 @@ CREATE TABLE users (
     FOREIGN KEY (person_id) REFERENCES persons(id)
 );
 
-CREATE TABLE childs (
+CREATE TABLE activities (
     id SERIAL PRIMARY KEY,
-    birthday_date DATE ,
-    gender_id INT ,
-    guard_id INT,
-    person_id INT,
-    child_id Varchar(50),
-    childminder_code Varchar(50),
-    parent_id INT,
-    FOREIGN KEY (gender_id) REFERENCES genders(id),
-    FOREIGN KEY (guard_id) REFERENCES guard_mode(id),
-    FOREIGN KEY (person_id) REFERENCES persons(id) 
+    activity_name VARCHAR(100)
 );
 
+CREATE TABLE leisures (
+    id SERIAL PRIMARY KEY,
+    leisure_name VARCHAR(100)
+);
+
+CREATE TABLE snacks (
+    id SERIAL PRIMARY KEY,
+    snack_name VARCHAR(100),
+    image_path VARCHAR(300)
+);
+
+CREATE TABLE histories (
+    id SERIAL PRIMARY KEY,
+    start_date TIMESTAMP,
+    end_date TIMESTAMP,
+    child_id INT,
+    childminder_id INT
+);
+
+CREATE TABLE childs (
+    id SERIAL PRIMARY KEY,
+    birthday_date DATE,
+    gender_id INT,
+    guard_id INT,
+    person_id INT,
+    childminder_code INT,
+    parent_id INT,
+    is_active BOOLEAN,
+    is_accepted BOOLEAN DEFAULT FALSE,
+    history_id INT,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (gender_id) REFERENCES genders(id),
+    FOREIGN KEY (guard_id) REFERENCES guard_mode(id),
+    FOREIGN KEY (person_id) REFERENCES persons(id),
+    FOREIGN KEY (history_id) REFERENCES histories(id)
+);
+
+CREATE TABLE childs_activities (
+    id SERIAL PRIMARY KEY,
+    activity_id INT,
+    child_id INT,
+    commentaire VARCHAR(300),
+    time TIME,
+    date DATE,
+    FOREIGN KEY (activity_id) REFERENCES activities(id),
+    FOREIGN KEY (child_id) REFERENCES childs(id)
+);
+
+CREATE TABLE childs_leisures (
+    id SERIAL PRIMARY KEY,
+    leisure_id INT,
+    child_id INT,
+    commentaire VARCHAR(300),
+    date DATE,
+    FOREIGN KEY (leisure_id) REFERENCES leisures(id),
+    FOREIGN KEY (child_id) REFERENCES childs(id)
+);
+
+CREATE TABLE meals (
+    id SERIAL PRIMARY KEY,
+    child_id INT,
+    snack_id INT,
+    eval VARCHAR(100),
+    commentaire VARCHAR(300),
+    date DATE,
+    type VARCHAR(100),
+    FOREIGN KEY (child_id) REFERENCES childs(id),
+    FOREIGN KEY (snack_id) REFERENCES snacks(id)
+);
+
+CREATE TABLE baby_bottles (
+    id SERIAL PRIMARY KEY,
+    child_id INT,
+    time VARCHAR(100),
+    date DATE,
+    volume DOUBLE PRECISION,
+    actuality_id INT
+);
+
+CREATE TABLE naps (
+    id SERIAL PRIMARY KEY,
+    child_id INT,
+    start_time VARCHAR(100),
+    end_time VARCHAR(100),
+    date DATE,
+    type VARCHAR(100),
+    commentaire VARCHAR(300),
+    actuality_id INT,
+    FOREIGN KEY (child_id) REFERENCES childs(id)
+);
+
+CREATE TABLE actualities (
+    id SERIAL PRIMARY KEY,
+    child_id INT,
+    date DATE,
+    child_activity_id INT,
+    baby_bottle_id INT,
+    meal_id INT,
+    snack_id INT,
+    nap_id INT,
+    leisure_id INT,
+    presence_id INT,
+    FOREIGN KEY (child_id) REFERENCES childs(id),
+    FOREIGN KEY (child_activity_id) REFERENCES childs_activities(id),
+    FOREIGN KEY (baby_bottle_id) REFERENCES baby_bottles(id),
+    FOREIGN KEY (meal_id) REFERENCES meals(id),
+    FOREIGN KEY (snack_id) REFERENCES snacks(id),
+    FOREIGN KEY (nap_id) REFERENCES naps(id),
+    FOREIGN KEY (leisure_id) REFERENCES childs_leisures(id)
+);
+
+CREATE TABLE password_reset_token (
+    id SERIAL PRIMARY KEY,
+    token VARCHAR(100),
+    email VARCHAR(100),
+    expiry_date TIMESTAMP
+);
+
+-- Add foreign key constraints between histories and childs tables
+ALTER TABLE histories
+    ADD CONSTRAINT fk_childs_histories
+    FOREIGN KEY (child_id) REFERENCES childs(id),
+    ADD CONSTRAINT fk_childminder_histories
+    FOREIGN KEY (childminder_id) REFERENCES persons(id);
 
 
