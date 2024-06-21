d5c9fecd1ef925034cb3e84a86441d1d0ad0eb44
Maroua SOLTANI <113924387+marouachd@users.noreply.github.com>
2024-06-21 10:58:09 +0200

    fix import

--- baby-care-hub-frontend/src/router/index.js
+++ baby-care-hub-frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Sidebar from "../components/Commons/Sidebar.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,7 +7,7 @@ const router = createRouter({
     {
       path: "/",
       name: "ma-journée",
-      component: () => import("../components/Commons/Sidebar.vue"),
+      component: Sidebar,
     },
     {
       path: "/ma-journee/general/:id",
